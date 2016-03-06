d498323a08a8b64d5ab6d2fc869cb5ff95152f33
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-06 13:53:47 +0100

    Add extension models for rules, search and players polls

---
+++ BggUwp/BggApi/Models/Abstract/PlayerPollResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BGGAPI.Models.Abstract;
+
+namespace BGGAPI.Models
+{
+    public class PlayerPollResult
+    {
+        public int NumberOfPlayers { get; set; }
+        public int Best { get; set; }
+        public int Recommended { get; set; }
+        public int NotRecommended { get; set; }
+        // If 4+ is recommended then NumberOfPlayers is 4 and this var is true
+        public bool NumberOfPlayersIsAndHigher { get; set; }
+    }
+}
---
+++ BggUwp/BggApi/Models/Abstract/RulesItem.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace BGGAPI.Models
+{
+    public class RulesItem
+    {
+        [JsonIgnore]
+        public Config ConfigHeader { get; set; }
+
+        public List<Weblink> WebLinks { get; set; }
+
+        public class Language
+        {
+            public object ObjectId { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Category
+        {
+            public object ObjectId { get; set; }
+            public string Name { get; set; }
+        }
+
+        public class Config
+        {
+            public List<Language> Languages { get; set; }
+            public List<Category> Categories { get; set; }
+            public int Endpage { get; set; }
+            public bool ShowControls { get; set; }
+        }
+
+        public class Weblink
+        {
+            public string Url { get; set; }
+            public string Name { get; set; }
+            public string Postdate { get; set; }
+            public string LinkId { get; set; }
+            public string LinkType { get; set; }
+            public string ObjectType { get; set; }
+            public string ObjectId { get; set; }
+            public string ItemState { get; set; }
+            public string RepImageId { get; set; }
+            public string ObjectLink { get; set; }
+            public List<string> Languages { get; set; }
+            public List<string> Categories { get; set; }
+        }
+    }
+}
