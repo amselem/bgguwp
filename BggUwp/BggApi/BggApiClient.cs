abe41d22d406a24a4d49fb206f4ce9c0d4b4c8c9
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-06 13:55:45 +0100

    Create download XML data function

--- BggUwp/BggApi/BggApiClient.cs
+++ BggUwp/BggApi/BggApiClient.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using BGGAPI.Models;
 
 namespace BggApi
 {
     public class BggApiClient
     {
+        private const string BASE_URL = "http://www.boardgamegeek.com/xmlapi2";
+
+        private async Task<XDocument> ReadData(Uri requestUrl)
+        {
+            // Due to malformed header I cannot use GetContentAsync and ReadAsStringAsync
+            // UTF-8 is now hard-coded...
+
+            XDocument data = null;
+            int retries = 0;
+            while (data == null && retries < 30)
+            {
+                retries++;
+                var request = WebRequest.CreateHttp(requestUrl);
+                request.ContinueTimeout = 15000;
+                using (var response = (HttpWebResponse)(await request.GetResponseAsync()))
+                {
+                    if (response.StatusCode == HttpStatusCode.Accepted)
+                    {
+                        await Task.Delay(100);
+                        continue;
+                    }
+                    using (var reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                    {
+                        data = XDocument.Parse(await reader.ReadToEndAsync());
+                    }
+                }
+            }
+
+            if (data != null)
+            {
+                return data;
+            }
+            else
+            {
+                throw new Exception("Failed to download BGG data.");
+            }
+        }
     }
 }
