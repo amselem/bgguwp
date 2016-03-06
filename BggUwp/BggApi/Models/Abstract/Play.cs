8d2bfd9a2ba1fdb8c6295a14668c96f5eb088252
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-06 13:52:35 +0100

    Add additional models - user, player, play

---
+++ BggUwp/BggApi/Models/Abstract/Play.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BGGAPI.Models.Abstract;
+
+namespace BGGAPI.Models
+{
+    public class Play 
+    {
+        public int PlayId { get; set; }
+        public string BoardGameName { get; set; }
+        public int BoardGameId { get; set; }
+        public int NumberOfPlays { get; set; }
+        public int Length { get; set; }
+        public string UserComment { get; set; }
+        public DateTime PlayDate { get; set; }
+        public List<Player> Players { get; set; }
+    }
+}
---
+++ BggUwp/BggApi/Models/Abstract/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BGGAPI.Models.Abstract;
+
+namespace BGGAPI.Models
+{
+    public class Player
+    {
+        public string Username { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+        public string StartPosition { get; set; }
+        public string Color { get; set; }
+        public int Score { get; set; }
+        public bool IsNewPlayer { get; set; } // Ideal toggle for creating new player profile
+        public bool IsWinner { get; set; }
+    }
+}
