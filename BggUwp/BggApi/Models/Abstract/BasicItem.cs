657f7d81cad708ce4b3ca38b3ac8c62feefd95cf
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-06 13:21:48 +0100

    Create abstract models

---
+++ BggUwp/BggApi/Models/Abstract/BasicItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGGAPI.Models.Abstract
+{
+    public abstract class BasicItem 
+    {
+        public int BoardGameId { get; set; }
+        public string Name { get; set; }
+        public int YearPublished { get; set; }
+        public string ThumbnailWeb { get; set; }
+    }
+}
