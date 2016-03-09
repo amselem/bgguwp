2149fe7d6fba10fff0ae97c442adc5f224fd3f57
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-09 19:30:38 +0100

    Add abstract data item models

---
+++ BggUwp/BggUwp/Data/Models/Abstract/BaseItem.cs
@@ -0,0 +1,61 @@
+﻿using SQLite.Net.Attributes;
+using Template10.Mvvm;
+
+namespace BggUwp.Data.Models.Abstract
+{
+    public abstract class BaseItem : ViewModelBase
+    {
+        private string _Name = "";
+        public string Name
+        {
+            get
+            {
+                return _Name;
+            }
+            set
+            {
+                Set(ref _Name, value);
+            }
+        }
+
+        private int _BoardGameId = 0;
+        [PrimaryKey]
+        public int BoardGameId
+        {
+            get
+            {
+                return _BoardGameId;
+            }
+            set
+            {
+                Set(ref _BoardGameId, value);
+            }
+        }
+
+        private string _Thumbnail = "";
+        public string Thumbnail
+        {
+            get
+            {
+                return _Thumbnail;
+            }
+            set
+            {
+                Set(ref _Thumbnail, value);
+            }
+        }
+
+        private int _YearPublished = 0;
+        public int YearPublished
+        {
+            get
+            {
+                return _YearPublished;
+            }
+            set
+            {
+                Set(ref _YearPublished, value);
+            }
+        }
+    }
+}
---
+++ BggUwp/BggUwp/Data/Models/Abstract/BoardGameItem.cs
@@ -0,0 +1,122 @@
+﻿namespace BggUwp.Data.Models.Abstract
+{
+    public abstract class BoardGameItem : BaseItem 
+    {
+        private string _Image = "";
+        public string Image
+        {
+            get
+            {
+                return _Image;
+            }
+            set
+            {
+                Set(ref _Image, value);
+            }
+        }
+
+        private int _MinPlayers = 0;
+        public int MinPlayers
+        {
+            get
+            {
+                return _MinPlayers;
+            }
+            set
+            {
+                Set(ref _MinPlayers, value);
+            }
+        }
+
+        private int _MaxPlayers = 0;
+        public int MaxPlayers
+        {
+            get
+            {
+                return _MaxPlayers;
+            }
+            set
+            {
+                Set(ref _MaxPlayers, value);
+            }
+        }
+
+        private int _PlayingTime = 0;
+        public int PlayingTime
+        {
+            get
+            {
+                return _PlayingTime;
+            }
+            set
+            {
+                Set(ref _PlayingTime, value);
+            }
+        }
+
+        private bool _IsExpansion = false;
+        public bool IsExpansion
+        {
+            get
+            {
+                return _IsExpansion;
+            }
+            set
+            {
+                Set(ref _IsExpansion, value);
+            }
+        }
+
+        private decimal _GeekRating = 0;
+        public decimal GeekRating
+        {
+            get
+            {
+                return _GeekRating;
+            }
+            set
+            {
+                Set(ref _GeekRating, value);
+            }
+        }
+
+        private decimal _AverageRating = 0;
+        public decimal AverageRating
+        {
+            get
+            {
+                return _AverageRating;
+            }
+            set
+            {
+                Set(ref _AverageRating, value);
+            }
+        }
+
+        private int _Rank = 0;
+        public int Rank
+        {
+            get
+            {
+                return _Rank;
+            }
+            set
+            {
+                Set(ref _Rank, value);
+            }
+        }
+
+        private int _NumberOfPlays = 0;
+        public int NumberOfPlays
+        {
+            get
+            {
+                return _NumberOfPlays;
+            }
+            set
+            {
+                Set(ref _NumberOfPlays, value);
+            }
+        }
+    }
+}
