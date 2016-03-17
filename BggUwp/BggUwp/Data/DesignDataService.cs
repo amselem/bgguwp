5a853b66cb92be02cf9432b4a890268b03d7218a
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-17 20:30:43 +0100

    Design HotItems view

---
+++ BggUwp/BggUwp/Data/DesignDataService.cs
@@ -0,0 +1,46 @@
+﻿using BggUwp.Data.Models;
+using BggUwp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BggUwp.Views
+{
+    public class DesignDataService
+    {
+        public DesignDataService()
+        {
+            LoadHotItemsList();
+        }
+
+        private HotItemsViewModel _HotItemsVM = new HotItemsViewModel();
+        public HotItemsViewModel HotItemsVM
+        {
+            get
+            {
+                return _HotItemsVM;
+            }
+        }
+
+        public void LoadHotItemsList()
+        {
+            HotDataItem item = new HotDataItem();
+            item.Name = "Design time HotItems item";
+            item.YearPublished = 1999;
+            HotItemsVM.HotItemsList.Add(item);
+
+            HotDataItem item2 = new HotDataItem();
+            item2.Name = "Design time HotItems item - second item";
+            item2.YearPublished = 2999;
+            HotItemsVM.HotItemsList.Add(item2);
+
+            HotDataItem item3 = new HotDataItem();
+            item3.Name = "Design time HotItems item - third item";
+            item3.YearPublished = 3999;
+            HotItemsVM.HotItemsList.Add(item3);
+        }
+    }
+}
--- BggUwp/BggUwp/Views/MainPage.xaml.cs
+++ BggUwp/BggUwp/Views/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 using System;
 using Windows.UI.Xaml.Controls;
+using BggApi;
 
 namespace BggUwp.Views
 {
@@ -9,6 +10,28 @@ namespace BggUwp.Views
         {
             InitializeComponent();
             NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+
+        private void HotItems_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            //var item = e.ClickedItem as HotDataItem;
+            //this.Frame.Navigate(typeof(BoardgamePage), item.BoardGameId);
+        }
+
+        private void Menu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //switch (((Pivot)sender).SelectedIndex)
+            //{
+            //    case 0:
+            //        FindAppBarButton.Visibility = Visibility.Visible;
+            //        FilterAppBarButton.Visibility = Visibility.Collapsed;
+            //        break;
+
+            //    case 1:
+            //        FindAppBarButton.Visibility = Visibility.Collapsed;
+            //        FilterAppBarButton.Visibility = Visibility.Visible;
+            //        break;
+            //}
         }
     }
 }
