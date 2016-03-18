01ef49fd765968ab1ed004459436809aa500cb90
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-18 14:36:19 +0100

    Design Collection View

--- BggUwp/BggUwp/Data/DesignDataService.cs
+++ BggUwp/BggUwp/Data/DesignDataService.cs
@@ -14,6 +14,7 @@ namespace BggUwp.Views
         public DesignDataService()
         {
             LoadHotItemsList();
+            LoadCollection();
         }
 
         private HotItemsViewModel _HotItemsVM = new HotItemsViewModel();
@@ -22,6 +23,15 @@ namespace BggUwp.Views
             get
             {
                 return _HotItemsVM;
+            }
+        }
+
+        private CollectionViewModel _CollectionVM = new CollectionViewModel();
+        public CollectionViewModel CollectionVM
+        {
+            get
+            {
+                return _CollectionVM;
             }
         }
 
@@ -41,6 +51,36 @@ namespace BggUwp.Views
             item3.Name = "Design time HotItems item - third item";
             item3.YearPublished = 3999;
             HotItemsVM.HotItemsList.Add(item3);
+        }
+
+        public void LoadCollection()
+        {
+            CollectionDataItem item = new CollectionDataItem();
+            item.Name = "Design time item";
+            item.YearPublished = 1999;
+            item.MaxPlayers = 7;
+            item.PlayingTime = 30;
+            item.Rank = 678;
+            item.Owned = true;
+            CollectionVM.FilteredCollection.Add(item);
+
+            CollectionDataItem item2 = new CollectionDataItem();
+            item2.Name = "Design time item - second item";
+            item2.YearPublished = 2999;
+            item2.MaxPlayers = 2;
+            item2.PlayingTime = 200;
+            item2.Rank = 1678;
+            item2.Owned = true;
+            CollectionVM.FilteredCollection.Add(item2);
+
+            CollectionDataItem item3 = new CollectionDataItem();
+            item3.Name = "Design time item - third item";
+            item3.YearPublished = 3999;
+            item3.MaxPlayers = 15;
+            item3.PlayingTime = 20;
+            item3.Rank = 58;
+            item3.Owned = true;
+            CollectionVM.FilteredCollection.Add(item3);
         }
     }
 }
