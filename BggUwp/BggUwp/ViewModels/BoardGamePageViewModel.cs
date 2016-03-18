cecd07ffac3ddeff9139e19f53a6ac9087813ffd
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-03-18 11:20:30 +0100

    Basic BoardGamePage for testing navigation

---
+++ BggUwp/BggUwp/ViewModels/BoardGamePageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using BggUwp.Data;
+using BggUwp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+using Windows.UI.Xaml.Navigation;
+
+namespace BggUwp.ViewModels
+{
+    public class BoardGamePageViewModel : ViewModelBase
+    {
+        private DataService dataService; 
+        Windows.UI.Core.CoreDispatcher dispatcher; 
+
+        public BoardGamePageViewModel()
+        {
+            if (!Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                dataService = new DataService();
+                dispatcher = Windows.UI.Core.CoreWindow.GetForCurrentThread().Dispatcher;
+            }
+        }
+
+        public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> suspensionState)
+        {
+            int gameId = (int)parameter;
+            await Task.CompletedTask;
+        }
+    }
+}
--- BggUwp/BggUwp/ViewModels/MainPageViewModel.cs
+++ BggUwp/BggUwp/ViewModels/MainPageViewModel.cs
@@ -1,8 +1,11 @@
+using BggUwp.Data.Models;
+using BggUwp.Data.Models.Abstract;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Template10.Mvvm;
 using Template10.Services.NavigationService;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace BggUwp.ViewModels
@@ -22,6 +25,9 @@ namespace BggUwp.ViewModels
                 return _HotItemsVM;
             }
         }
+
+        public void GoToBoardGamePage(object sender, ItemClickEventArgs e) =>
+    NavigationService.Navigate(typeof(Views.BoardGamePage), ((BaseItem)e.ClickedItem).BoardGameId);
     }
 }
 
