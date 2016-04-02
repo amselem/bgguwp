cea5ab4569444b396ad7989a1198fca10f6175b8
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-04-02 10:55:31 +0200

    Add basic info display

---
+++ BggUwp/BggUwp/Common/Converters/ListToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BggUwp.Common.Converters
+{
+    public class ListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            IList<string> list = value as IList<string>;
+
+            if (list == null)
+                return String.Empty;
+
+            if (list.Count > 4)
+            {
+                return string.Join(Environment.NewLine, list.Take(4)) + Environment.NewLine + "...";
+            }
+
+            return string.Join(Environment.NewLine, list);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
