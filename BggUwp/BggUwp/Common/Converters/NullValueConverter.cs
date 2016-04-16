470c84c92ac63e84c0194d7510f87ef048c3b9b1
Paweł Rzepiński <rzepinski.pawel@gmail.com>
2016-04-16 20:29:29 +0200

    Add null value converter

---
+++ BggUwp/BggUwp/Common/Converters/NullValueConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace BggUwp.Common.Converters
+{
+    public class NullValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+                return NullString;
+
+            if (Double.Parse(value.ToString()) == 0)
+                return NullString;
+
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string NullString { get; set; }
+    }
+}
